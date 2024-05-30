6ae842195428b3f0d7b0704f2842733ac1fdd7a2
samba-space <junkipark430@gmail.com>
2024-05-30 21:11:36 +0900

    feat: Brand 저장 api 구현
    - Brand 저장 기능 구현
    - Brand 저장 기능 unit구현

---
+++ src/main/java/com/musinsa/brand/presentation/BrandController.java
@@ -0,0 +1,28 @@
+package com.musinsa.brand.presentation;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.musinsa.brand.application.BrandSaveRequest;
+import com.musinsa.brand.application.BrandService;
+
+@RestController
+public class BrandController {
+    private final BrandService brandService;
+
+    public BrandController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @PostMapping(path = "/api/v1/brands")
+    public ResponseEntity<Void> saveBrand(@RequestBody @Validated BrandSaveRequest request) {
+        Long saveBrandId = brandService.saveBrand(request);
+        return ResponseEntity.created(URI.create(String.format("/api/v1/brands/%d", saveBrandId)))
+                .build();
+    }
+}
---
+++ src/test/java/com/musinsa/brand/presentation/BrandControllerUnitTest.java
@@ -0,0 +1,51 @@
+package com.musinsa.brand.presentation;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musinsa.brand.application.BrandSaveRequest;
+import com.musinsa.brand.application.BrandService;
+
+@WebMvcTest(BrandController.class)
+class BrandControllerUnitTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @Autowired private ObjectMapper objectMapper;
+
+    @MockBean private BrandService brandService;
+
+    @DisplayName("브랜드 추가")
+    @Test
+    void test() throws Exception {
+        // given
+        String brandName = "musinsa";
+        BrandSaveRequest request = new BrandSaveRequest(brandName);
+        when(brandService.saveBrand(any())).thenReturn(1L);
+
+        // when
+        ResultActions result =
+                mockMvc.perform(
+                        post("/api/v1/brands")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON));
+        // then
+        result.andExpect(status().isCreated())
+                .andExpect(header().string(HttpHeaders.LOCATION, "/api/v1/brands/1"));
+    }
+}
