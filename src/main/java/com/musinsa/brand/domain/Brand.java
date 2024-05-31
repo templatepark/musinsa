db65ad4424b3006e1d92b601a6402badf42a69c4
samba-space <junkipark430@gmail.com>
2024-05-31 16:47:55 +0900

    config: h2 mysql mode로 변경
    - application.yml h2 설정 변경
    - Brand 도메인 id strategy IDENTITY로 변경

--- src/main/java/com/musinsa/brand/domain/Brand.java
+++ src/main/java/com/musinsa/brand/domain/Brand.java
@@ -1,14 +1,15 @@
 package com.musinsa.brand.domain;
 
-import com.musinsa.common.domain.BaseTimeEntity;
 import jakarta.persistence.*;
+
+import com.musinsa.common.domain.BaseTimeEntity;
 
 @Entity
 @Table(name = "brand")
 public class Brand extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "brand_id")
     private Long id;
 
