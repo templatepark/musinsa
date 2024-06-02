e87ec72fcb86ea8d2629e25aed66a115d6129c23
samba-space <junkipark430@gmail.com>
2024-06-03 00:12:59 +0900

    feat: 카테고리 이름으로 최저, 최고 가격 브랜드, 가격 조회 query api 구현

--- src/main/java/com/musinsa/product/presentation/ProductQueryController.java
+++ src/main/java/com/musinsa/product/presentation/ProductQueryController.java
@@ -1,9 +1,13 @@
 package com.musinsa.product.presentation;
 
+import jakarta.validation.constraints.NotBlank;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.musinsa.product.application.CategoryLowestAndHighestPriceResponse;
 import com.musinsa.product.application.CategoryLowestPriceResponse;
 import com.musinsa.product.application.ProductQueryService;
 
@@ -19,5 +23,13 @@ public class ProductQueryController {
     @GetMapping(path = "/api/v1/products/category-lowest-prices")
     public ResponseEntity<CategoryLowestPriceResponse> getCategoryLowestPrices() {
         return ResponseEntity.ok().body(productQueryService.getCategoryLowestPrices());
+    }
+
+    @GetMapping(path = "/api/v1/products/category-lowest-highest-prices")
+    public ResponseEntity<CategoryLowestAndHighestPriceResponse>
+            getLowestAndHighestPricesByCategoryName(
+                    @RequestParam(name = "categoryName") @NotBlank String categoryName) {
+        return ResponseEntity.ok()
+                .body(productQueryService.getLowestAndHighestPricesByCategoryName(categoryName));
     }
 }
