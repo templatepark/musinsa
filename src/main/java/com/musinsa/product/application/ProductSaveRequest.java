979e420013b1d0a38449edf96847fc154f88dc6f
samba-space <junkipark430@gmail.com>
2024-06-03 21:50:58 +0900

    fix: product 저장 시, 요청 필드 null check 추가

--- src/main/java/com/musinsa/product/application/ProductSaveRequest.java
+++ src/main/java/com/musinsa/product/application/ProductSaveRequest.java
@@ -3,12 +3,15 @@ package com.musinsa.product.application;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 import com.musinsa.product.domain.Money;
 import com.musinsa.product.domain.Product;
 
 public record ProductSaveRequest(
-        @Min(1L) Long brandId, @Min(1L) Long categoryId, @Min(1L) BigDecimal price) {
+        @NotNull @Min(1L) Long brandId,
+        @NotNull @Min(1L) Long categoryId,
+        @NotNull @Min(1L) BigDecimal price) {
 
     public Product toProduct() {
         return new Product(categoryId, brandId, new Money(price));
