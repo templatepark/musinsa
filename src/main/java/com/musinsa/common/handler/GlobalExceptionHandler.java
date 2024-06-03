f0dd319e40b84fc3b49f9373cc3c42944874e095
samba-space <junkipark430@gmail.com>
2024-06-04 02:05:05 +0900

    feat: 공통 에러처리
    - GlobalException handler 구현

---
+++ src/main/java/com/musinsa/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.musinsa.common.handler;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+
+import com.musinsa.brand.application.AlreadyBrandNameException;
+import com.musinsa.product.application.NotExistBrandException;
+import com.musinsa.product.application.NotExistCategoryException;
+import com.musinsa.product.application.NotExistProductException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AlreadyBrandNameException.class)
+    public ErrorResponse handleNotFoundException(AlreadyBrandNameException e) {
+        return ErrorResponse.create(e, HttpStatus.CONFLICT, e.getMessage());
+    }
+
+    @ExceptionHandler({
+        NotExistBrandException.class,
+        NotExistCategoryException.class,
+        NotExistProductException.class
+    })
+    public ErrorResponse handleNotExistException(RuntimeException e) {
+        return ErrorResponse.create(e, HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ErrorResponse handleArgumentNotValidationException(MethodArgumentNotValidException e) {
+        String errorMessages =
+                e.getBindingResult().getFieldErrors().stream()
+                        .map(
+                                error ->
+                                        String.join(
+                                                ": ", error.getField(), error.getDefaultMessage()))
+                        .collect(Collectors.joining("; "));
+
+        return ErrorResponse.create(e, e.getStatusCode(), errorMessages);
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ErrorResponse handleMethodValidationException(HandlerMethodValidationException e) {
+        return ErrorResponse.create(e, e.getStatusCode(), e.getMessage());
+    }
+}
